4c0302627a3def7d8c1f5e04ad437d448a2f884b
Anel <ac@anelcanto.com>
2022-10-13 01:54:44 +0000

    Add index, create, and new actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,21 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+    
+    def create
+        @post = Post.new task_params
+        if @post.save
+            redirect_to posts_path, notice: "Task created."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
+    def show
+        @post = Post.find params[:id]
+    end
+    
+    def task_params
+        params.require(:post).permit(:content, :parent_id)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    validates :content, presence: true
     has_many :replies, class_name: "Post", foreign_key: "parent_id"
 
     belongs_to :parent, class_name: "Post", optional: true
