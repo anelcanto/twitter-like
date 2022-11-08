f8f450f5f440d617ab764bb859e7160096f9c484
Anel <ac@anelcanto.com>
2022-11-08 00:45:28 +0000

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
-  root 'posts#index'
-  resources :posts do
+  get "signup" => "users#new"
+  get "profile" => "users#show"
+  resources :users, except: [:index] 
+  resources :posts do 
     resources :comments
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'about' => "sites#about"
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'posts#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_035804) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_05_053435) do
   create_table "comments", force: :cascade do |t|
     t.string "tittle"
     t.text "content"
@@ -29,6 +29,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_035804) do
     t.string "mood"
     t.string "country"
     t.index ["parent_id"], name: "index_posts_on_parent_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "posts", "posts", column: "parent_id"
